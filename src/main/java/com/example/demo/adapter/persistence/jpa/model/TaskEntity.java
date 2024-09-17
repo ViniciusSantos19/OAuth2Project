c134c313c54581232fb9c8ba2a94690f86bf1a3f
ViniciusSantos19 <vinicontatosq@gmail.com>
2024-09-17 18:06:41 -0300

    task entity

--- src/main/java/com/example/demo/adapter/persistence/jpa/model/TaskEntity.java
+++ src/main/java/com/example/demo/adapter/persistence/jpa/model/TaskEntity.java
@@ -1,4 +1,56 @@
 package com.example.demo.adapter.persistence.jpa.model;
 
+import com.example.demo.domain.task.TaskPriority;
+import com.example.demo.domain.task.TaskStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "tasks")
 public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private TaskPriority priority;
+
+    private LocalDateTime dueDate;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "task_users", // Join table name
+            joinColumns = @JoinColumn(name = "task_id"), // Column for this entity's ID
+            inverseJoinColumns = @JoinColumn(name = "user_id") // Column for the associated entity's ID
+    )
+    private Set<UserEntity> members;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
